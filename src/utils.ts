a614793e2ddbd92535d871272021158074f4d757
Nacho Elias <ignaciomartinelias@gmail.com>
2024-11-28 11:47:52 +0100

    add extra helper to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,35 @@
 import { SearchResult } from "./types";
 
+const metadataMap = {
+  description: {
+    attribute: "name",
+    value: "description",
+  },
+  image: {
+    attribute: "property",
+    value: "og:image",
+  },
+};
+
+// This helper retrieves all metadata using a metadataMap
+export const getMetadataValuesFromAttributes = (data: SearchResult) =>
+  Object.entries(metadataMap).reduce(
+    (acc, [label, value]) => {
+      return {
+        ...acc,
+        [label]: data.metadata
+          .find((meta) => meta.selector === label)
+          ?.matches.find(
+            (match) => match.attr?.[value.attribute] === value.value
+          )?.attr?.content,
+      };
+    },
+    {} as {
+      [K in keyof typeof metadataMap]?: string;
+    }
+  );
+
+// This helper retrieves each metadata individually
 export const getMetadataValues = (data: SearchResult) => {
   const descriptionMetadata = data.metadata.find(
     (meta) => meta.selector === "description"
