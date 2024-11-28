8664899cf809e948ac968b9007ed40a282503dcd
Nacho Elias <ignaciomartinelias@gmail.com>
2024-11-28 11:32:35 +0100

    add specific match for each metadata item

--- src/utils.ts
+++ src/utils.ts
@@ -7,8 +7,13 @@ export const getMetadataValues = (data: SearchResult) => {
 
   const imageMetadata = data.metadata.find((meta) => meta.selector === "image");
 
-  const description = descriptionMetadata?.matches[0].attr?.content || null;
-  const image = imageMetadata?.matches[0].attr?.content || null;
+  const description =
+    descriptionMetadata?.matches.find(
+      (match) => match.attr?.name === "description"
+    )?.attr?.content || null;
+  const image =
+    imageMetadata?.matches.find((match) => match.attr?.property === "og:image")
+      ?.attr?.content || null;
 
   return { description, image };
 };
